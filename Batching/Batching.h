1938c49f6bf8e2e9fe829caba3cb250e4fccdc58
Jatin Arora <jatin.ppe@jatin-macbook-pro.local>
2017-01-10 15:16:26 +0530

    Initial Commit

---
+++ Batching/Batching.h
@@ -0,0 +1,19 @@
+//
+//  Batching.h
+//  Batching
+//
+//  Created by Jatin Arora on 10/01/17.
+//  Copyright © 2017 Jatin Arora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Batching.
+FOUNDATION_EXPORT double BatchingVersionNumber;
+
+//! Project version string for Batching.
+FOUNDATION_EXPORT const unsigned char BatchingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Batching/PublicHeader.h>
+
+
