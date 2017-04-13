484b74ab45060c17ba9ddc29fde6c9a8b312524a
Jatin Arora <jatinarora269@gmail.com>
2017-04-13 11:59:30 +0530

    Add demo project

---
+++ BatchingDemo/BatchingDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BatchingDemo
+//
+//  Created by Jatin Arora on 13/04/17.
+//  Copyright © 2017 Jatin Arora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
