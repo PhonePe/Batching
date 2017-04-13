79055d46ef716e770cad91ade0a614498d9d43d3
Jatin Arora <jatinarora269@gmail.com>
2017-04-13 05:46:23 +0530

    Add Event model class

---
+++ Batching/Persistence/Event+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Event+CoreDataClass.swift
+//  Batching
+//
+//  Created by Jatin Arora on 13/04/17.
+//  Copyright © 2017 Jatin Arora. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Event)
+public class Event: NSManagedObject {
+
+}
---
+++ Batching/Persistence/Event+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Event+CoreDataProperties.swift
+//  Batching
+//
+//  Created by Jatin Arora on 13/04/17.
+//  Copyright © 2017 Jatin Arora. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Event {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Event> {
+        return NSFetchRequest<Event>(entityName: "Event")
+    }
+
+    @NSManaged public var data: NSData?
+    @NSManaged public var id: String?
+
+}
