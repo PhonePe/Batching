6951cd5de358ff4608c31c8f598eb28da6c2a6f5
Jatin Arora <jatinarora269@gmail.com>
2017-04-17 17:27:14 +0530

    Add test target to demo project

---
+++ Demo/DemoTests/DemoTests.swift
@@ -0,0 +1,37 @@
+//
+//  DemoTests.swift
+//  DemoTests
+//
+//  Created by Jatin Arora on 17/04/17.
+//  Copyright © 2017 Jatin Arora. All rights reserved.
+//
+
+import XCTest
+import Batching
+
+class DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        
+        
+        
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
